dcead851c9e70c3d45da4b47d45d16f419a91e01
ajiiz <opticpiter@gmail.com>
2020-08-25 12:52:06 +0200

    Starting to make menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../styles/App.css'
+import Menu from './Menu'
 
 const App = () => {
     return (
         <main>
-
+            <Menu />
         </main>
     )
 }
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Menu = () => {
+    return (
+        <div className="menu">
+            <Logo />
+            <MenuOptions />
+        </div>
+    )
+}
+
+export default Menu
