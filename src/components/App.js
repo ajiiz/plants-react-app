faded3e36e541d9986620cd1a72ed0cfd8610e59
ajiiz <opticpiter@gmail.com>
2020-08-26 12:05:45 +0200

    Added background and border to main page, changed design

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,23 @@ import React from 'react'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import '../styles/App/App.css'
 import Menu from './NavigationBar/Menu'
-import About from './About'
-import Plants from './Plants'
+import About from './HomePage/About'
+import Plants from './PlantsPage/Plants'
 
 const App = () => {
     return (
-        <Router>
-            <main>
-                <Menu />
-                <Switch>
-                    <Route exact path="/" component={About} />
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/plants" component={Plants} />
-                </Switch>
-            </main>
-        </Router>
+        <header>
+            <Router>
+                <nav>
+                    <Menu />
+                </nav>
+                    <Switch>
+                        <Route exact path="/" component={About} />
+                        <Route exact path="/about" component={About} />
+                        <Route exact path="/plants" component={Plants} />
+                    </Switch>
+            </Router>
+        </header>
     )
 }
 
