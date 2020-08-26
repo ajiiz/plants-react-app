14360027429bd5225e1ba7fb8c33ef5ba562beff
ajiiz <opticpiter@gmail.com>
2020-08-26 16:01:55 +0200

    Added title and subtitle in header, last to do is small hr

--- src/components/HomePage/Header.js
+++ src/components/HomePage/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <header>
             <div className="wrapper">
                 <h1 className="title">plant<span>s</span>pot</h1>
-                <h3 className="subtitle">subtitle</h3>
+                <h3 className="subtitle">healthy breath</h3>
             </div>
         </header>
     )
