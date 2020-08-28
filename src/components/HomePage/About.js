3b21b67072f7e83043d55cd739bd853609018946
ajiiz <opticpiter@gmail.com>
2020-08-28 16:16:32 +0200

    Completed styling about section

--- src/components/HomePage/About.js
+++ src/components/HomePage/About.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import '../../styles/Home/about.css'
+import Image from '../../assets/image-about-my-2.jpg'
 
 const About = () => {
     return (
         <div className="about">
+            <div className="about__imagewrapper"></div>
+            <div className="about__textcontainer">
+                <h3 className="header">master the green</h3>
+                <hr className="divider"></hr>
+                <div className="text"><p>
+                A wonderful serenity has taken possession
+                of my entire
+                soul, like these sweet mornings of
+                spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now.
+                </p></div>
+            </div>
         </div>
     )
 }
