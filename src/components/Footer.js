ff0b703f8346bb553b7fd1cbc2be788cf275a25e
ajiiz <opticpiter@gmail.com>
2020-08-29 16:00:20 +0200

    Added footer structure

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import '../styles/Footer/footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="footer">
+            <div className="footer__icons">
+                <i class="fab fa-instagram-square"></i>
+            </div>
+            <div className="footer__menu">
+            </div>
         </div>
     )
 }
