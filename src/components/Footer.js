eb9761885bdbd042706b44a28bd60fb7962ffd9c
ajiiz <opticpiter@gmail.com>
2020-08-30 23:28:13 +0200

    Installed FontAwesome brand icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import '../styles/Footer/footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagramSquare } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
     return (
         <div className="footer">
             <div className="footer__icons">
-                <i class="fab fa-instagram-square"></i>
+                <FontAwesomeIcon icon={ faInstagramSquare } />
             </div>
             <div className="footer__menu">
             </div>
