cd725074b26edcfde619a094680cd0318836f642
ajiiz <opticpiter@gmail.com>
2020-08-31 00:21:05 +0200

    Added baisc footer structure

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import '../styles/Footer/footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagramSquare, faFacebookSquare, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+import { faInstagramSquare, faFacebookSquare, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faCircle } from '@fortawesome/fontawesome-free-regular'
 
 const Footer = () => {
 
@@ -10,15 +11,22 @@ const Footer = () => {
     }
 
     return (
-        <div className="footer">
-            <div className="footer__icons">
-                <FontAwesomeIcon style={styles} icon={ faInstagramSquare } />
-                <FontAwesomeIcon style={styles} icon={ faFacebookSquare } />
-                <FontAwesomeIcon style={styles} icon={ faGithubSquare } />
+        <footer>
+            <div className="contact">
+                <div className="contacttext"><h3>Contact Me:</h3></div>
+                <div className="contact__icons">
+                    <span className="fa-layers fa-fw">
+                        <FontAwesomeIcon style={styles} icon={ faInstagramSquare } size="lg" />
+                        <FontAwesomeIcon icon={ faCircle} size="lg" />
+                    </span>
+                    <FontAwesomeIcon style={styles} icon={ faFacebookSquare } size="lg" />
+                        <FontAwesomeIcon style={styles} icon={ faGithub } size="lg" />
+                </div>
             </div>
-            <div className="footer__menu">
+            <div className="copyright">
+                <h3>Made by Piotr Wrobel 2020</h3>
             </div>
-        </div>
+        </footer>
     )
 }
 
