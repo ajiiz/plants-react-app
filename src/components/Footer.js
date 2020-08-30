85d6c9705e890ea1e20db1acdaac55d88416e067
ajiiz <opticpiter@gmail.com>
2020-08-31 00:39:10 +0200

    Styled footer contact me section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,14 +13,20 @@ const Footer = () => {
     return (
         <footer>
             <div className="contact">
-                <div className="contacttext"><h3>Contact Me:</h3></div>
                 <div className="contact__icons">
-                    <span className="fa-layers fa-fw">
+                    <div className="contact__text"><h3>Contact Me</h3></div>
+                    <div className="contact__icons__link">
                         <FontAwesomeIcon style={styles} icon={ faInstagramSquare } size="lg" />
-                        <FontAwesomeIcon icon={ faCircle} size="lg" />
-                    </span>
-                    <FontAwesomeIcon style={styles} icon={ faFacebookSquare } size="lg" />
+                        <div className="contact__icons__link__text">DM me on Instagram.</div>
+                    </div>
+                    <div className="contact__icons__link">
+                        <FontAwesomeIcon style={styles} icon={ faFacebookSquare } size="lg" />
+                        <div className="contact__icons__link__text">Add me on Facebook.</div>
+                    </div>
+                    <div className="contact__icons__link">
                         <FontAwesomeIcon style={styles} icon={ faGithub } size="lg" />
+                        <div className="contact__icons__link__text">Check my GitHub.</div>
+                    </div>
                 </div>
             </div>
             <div className="copyright">
