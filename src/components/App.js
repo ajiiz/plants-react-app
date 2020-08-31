c154a2d434fba3201286464ee9947f7bd09f9fd7
ajiiz <opticpiter@gmail.com>
2020-08-31 12:17:52 +0200

    Deleted all css files

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
-import '../styles/App/App.css'
+import '../styles/App/app.scss'
 import Menu from './NavigationBar/Menu'
 import Home from './HomePage/Home'
 import Plants from './PlantsPage/Plants'
--- src/components/PlantsPage/Plants.js
+++ src/components/PlantsPage/Plants.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import "../../styles/Plants/plants.css"
+import "../../styles/Plants/plants.scss"
 
 const Plants = () => {
     return (
