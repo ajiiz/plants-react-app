3619ca28a592e829010aa4c965dff80e57674994
ajiiz <opticpiter@gmail.com>
2020-09-01 16:49:03 +0200

    Added hamburger icon with animation in nav bar

--- src/components/NavigationBar/Hamburger.js
+++ src/components/NavigationBar/Hamburger.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Hamburger = () => {
-    return (
-        <div>
 
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleClick = () => {
+        setIsOpen(!isOpen)
+    }
+
+    return (
+        <div className={!isOpen ? "menu__btn" : "menu__btn menu__btn__open"} onClick={handleClick}>
+            <div className="menu__btn__burger"></div>
         </div>
     )
 }
