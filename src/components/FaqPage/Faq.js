d01f62bdaab3cd1c1d9b0cab3cf1567dc5df24e6
ajiiz <opticpiter@gmail.com>
2020-09-09 20:00:12 +0200

    Imported new component to Faq component

--- src/components/FaqPage/Faq.js
+++ src/components/FaqPage/Faq.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import NavigationBar from '../NavigationBar/Menu'
+import FaqContent from './FaqContent'
 
 const Faq = () => {
     return (
         <div className="faq">
             <div className="faq__image"></div>
             <div className="faq__content">
-
+                <FaqContent />
             </div>
         </div>
     )
