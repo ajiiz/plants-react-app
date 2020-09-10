1d7489617140fc3ce51bf29768e55aefadf1ed0c
ajiiz <opticpiter@gmail.com>
2020-09-11 01:32:56 +0200

    Added answer styling in FaqContent component

--- src/components/FaqPage/FaqContent.js
+++ src/components/FaqPage/FaqContent.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
-const FaqContent = ({ question, answer, key }) => {
-
-    const clicked = () => {
-        console.log()
-    }
-
+const FaqContent = ({ question, answer }) => {
     return (
         <div className="faq__content__wrapper">
             <h3 className="faq__content__wrapper__question">
-                <span>{question[0]}</span>{question.substr(1,question.length-2)}
-                <span>{question[question.length-1]}</span></h3>
-            <p className="faq__content__wrapper__answer">{answer}</p>
-            <hr className="faq__content__wrapper__divider" onClick={() => clicked}></hr>
+                <span>{question[0]}</span>
+                {question.substr(1,question.length-2)}
+                <span>{question[question.length-1]}</span>
+            </h3>
+            <p className="faq__content__wrapper__answer">
+                <span>{answer[0]}</span>
+                {answer.substr(1,answer.length)}
+            </p>
+            <hr className="faq__content__wrapper__divider"></hr>
         </div>
     )
 }
