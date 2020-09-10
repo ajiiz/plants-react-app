7ab0dfaacbb223df0ea8d8581a6808a181437c86
ajiiz <opticpiter@gmail.com>
2020-09-11 00:22:26 +0200

    Added styling to first and last letter in FaqContent component and started to style content

--- src/components/FaqPage/FaqContent.js
+++ src/components/FaqPage/FaqContent.js
@@ -3,7 +3,9 @@ import React from 'react'
 const FaqContent = ({ question, answer }) => {
     return (
         <div className="faq__content__wrapper">
-            <h3 className="faq__content__wrapper__question">{question}</h3>
+            <h3 className="faq__content__wrapper__question">
+                <span>{question[0]}</span>{question.substr(1,question.length-2)}
+                <span>{question[question.length-1]}</span></h3>
             <p className="faq__content__wrapper__answer">{answer}</p>
         </div>
     )
