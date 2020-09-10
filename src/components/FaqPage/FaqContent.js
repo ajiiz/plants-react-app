fc9d2d4cf04ace0e77a4dbf24ff43c5a360a0939
ajiiz <opticpiter@gmail.com>
2020-09-11 00:12:52 +0200

    Added content wrapper in FaqContent component and styled display in Faq component

--- src/components/FaqPage/FaqContent.js
+++ src/components/FaqPage/FaqContent.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const FaqContent = ({ question, answer }) => {
     return (
-        <>
-            <h3 className="faq__content__question">{question}</h3>
-            <p className="faq__content__answer">{answer}</p>
-        </>
+        <div className="faq__content__wrapper">
+            <h3 className="faq__content__wrapper__question">{question}</h3>
+            <p className="faq__content__wrapper__answer">{answer}</p>
+        </div>
     )
 }
 
