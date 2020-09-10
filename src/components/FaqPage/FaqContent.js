2ba2fa263797255825a16ceaa9fb237a0014ae64
ajiiz <opticpiter@gmail.com>
2020-09-11 01:20:28 +0200

    Added divider styling and removed divider in last child with q&a in FaqContent component

--- src/components/FaqPage/FaqContent.js
+++ src/components/FaqPage/FaqContent.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
-const FaqContent = ({ question, answer }) => {
+const FaqContent = ({ question, answer, key }) => {
+
+    const clicked = () => {
+        console.log()
+    }
+
     return (
         <div className="faq__content__wrapper">
             <h3 className="faq__content__wrapper__question">
                 <span>{question[0]}</span>{question.substr(1,question.length-2)}
                 <span>{question[question.length-1]}</span></h3>
             <p className="faq__content__wrapper__answer">{answer}</p>
-            <hr className="faq__content__wrapper__divider"></hr>
+            <hr className="faq__content__wrapper__divider" onClick={() => clicked}></hr>
         </div>
     )
 }
