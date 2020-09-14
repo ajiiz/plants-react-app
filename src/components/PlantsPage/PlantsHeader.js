e521059d3c18dd32a592166b2fb61136ce0fedb2
ajiiz <opticpiter@gmail.com>
2020-09-14 22:55:00 +0200

    Added styled title and styled headerImage in PlantsHeader component

--- src/components/PlantsPage/PlantsHeader.js
+++ src/components/PlantsPage/PlantsHeader.js
@@ -3,9 +3,9 @@ import React from 'react'
 const PlantsHeader = () => {
     return (
         <div className="plants__header">
-            <h3 className="plants__header__title">frequently</h3>
-            <h3 className="plants__header__title">asked</h3>
-            <h3 className="plants__header__title">questions</h3>
+            <h3 className="plants__header__title">search</h3>
+            <h3 className="plants__header__title">for</h3>
+            <h3 className="plants__header__title">plants</h3>
             <hr className="plants__header__divider"></hr>
         </div>
     )
