28a7439e0ec89d3a60f447bf9219226c119b6810
ajiiz <opticpiter@gmail.com>
2020-09-16 17:45:07 +0200

    Added conditional rendering for button `next` and `previous` in PlantsPageSelect component

--- src/components/PlantsPage/PlantsItems.js
+++ src/components/PlantsPage/PlantsItems.js
@@ -3,7 +3,7 @@ import React from 'react'
 const PlantsItems = ({ common_name, img_url, scientific_name, genus, slug}) => {
     return (
         <div className="plants__container__content__plant" onClick={()=>console.log(img_url)}>
-            <img src={img_url} style={{width:'100px', height:'100px'}}></img>
+            <img alt="" src={img_url} style={{width:'100px', height:'100px'}}></img>
             <p>common_name: {common_name}</p>
             <p>scientific_name: {scientific_name}</p>
             <p>genus: {genus}</p>
--- src/components/PlantsPage/PlantsPageSelect.js
+++ src/components/PlantsPage/PlantsPageSelect.js
@@ -3,13 +3,21 @@ import React from 'react'
 const PlantsPageSelect = ({ page, changePage }) => {
     return (
         <div plants__page-buttons>
+            <button
+                style={{display: (page===1) ? 'none' : 'inline'}}
+                onClick={()=>changePage(page - 1)}>
+                Previous
+            </button>
             <button onClick={()=>changePage(1)}>First</button>
-            <button onClick={()=>changePage(page - 1)}>&lt;-</button>
             <button onClick={()=>changePage(page)}>{ page }</button>
             <button onClick={()=>changePage(page + 1)}>{ page + 1}</button>
             <button onClick={()=>changePage(page + 2)}>{ page + 2}</button>
-            <button onClick={()=>changePage(page + 1)}>-&gt;</button>
-            <button>Last</button>
+            <button onClick={()=>changePage(256)}>Last</button>
+            <button
+                style={{display: (page===256) ? 'none' : 'inline'}}
+                onClick={()=>changePage(page + 1)}>
+                Next
+            </button>
         </div>
     )
 }
