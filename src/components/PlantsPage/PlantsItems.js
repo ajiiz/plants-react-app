cbd8cdbfcdf4827ae228fbf34ac488eba67587bc
ajiiz <opticpiter@gmail.com>
2020-09-18 17:48:22 +0200

    Deleted method toUpper and modified checkName so it returns already toUpper text and checks if name is defined in PlantsItems component

--- src/components/PlantsPage/PlantsItems.js
+++ src/components/PlantsPage/PlantsItems.js
@@ -7,29 +7,25 @@ const PlantsItems = ({ commonName, imgUrl, scientificName, family, synonyms, aut
         backgroundImage: (imgUrl!==null) ? `url(${imgUrl})` : `url(${ImageNotFound})`
     }
 
-    const checkText = (text) => {
+    const checkName = (text) => {
         if(text === null) {
             text = scientificName
         } else if(scientificName === null) {
-            text = "Not Found"
+            return text = "[not found]"
         }
-        return text
+        return text.charAt(0).toUpperCase() + text.slice(1)
     }
 
-    const firstUpper = (text) => {
-        if(text !== null) return text.charAt(0).toUpperCase() + text.slice(1)
-        else return ""
-    }
-
-    const lowestLength = (arr) => {
+    const lowestLengthSyn = (arr) => {
         let syn = ""
         if(arr.length !== 0) {
             let minLen = Number.POSITIVE_INFINITY
             arr.forEach(el => {
                 syn = (el.length < minLen) ? el : ""
             });
-        }
-        return syn
+            return syn
+        } else return "[not found]"
+
     }
     return (
         <div className="plants__container__items__item">
@@ -40,18 +36,21 @@ const PlantsItems = ({ commonName, imgUrl, scientificName, family, synonyms, aut
             <div className="plants__container__items__item__content">
                 <div className="plants__container__items__item__content__wrapper">
                     <h3 className="plants__container__items__item__content__wrapper__name">
-                        {firstUpper(scientificName)}
+                        {checkName(scientificName)}
                     </h3>
-                    <p className="plants__container__items__item__content__wrapper__info">Also called {firstUpper(commonName)}.
-                        Is a spiecies of the
+                    <p className="plants__container__items__item__content__wrapper__info">
+                        Also called {checkName(commonName)}.
+                        It is a spiecies of the
                         <span className="plants__container__items__item__content__wrapper__info__family">
-                            {family}</span> family.
+                            {" "+family}</span> family.
                     </p>
                     <p className="plants__container__items__item__content__wrapper__synonyms">
-                        synonyms: {lowestLength(synonyms)}, ...
+                        synonyms
                         <span className="plants__container__items__item__content__wrapper__synonyms__length">
-                            ({(synonyms.length !== undefined) ? synonyms.length : null})
-                        </span>
+                            {(synonyms.length !== 0) ? "("+synonyms.length+")" : null}
+                        </span>:
+                        {" "+lowestLengthSyn(synonyms)}
+
                     </p>
                     <p className="plants__container__items__item__content__wrapper__author">
                         [author: {author}]
