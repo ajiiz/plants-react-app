52f2c92ddf688f7bf4784a1a2c9f9c358c38d931
ajiiz <opticpiter@gmail.com>
2020-09-19 15:00:50 +0200

    Changed a bit classNames in QuoteItems component

--- src/components/HomePage/QuoteItem.js
+++ src/components/HomePage/QuoteItem.js
@@ -3,13 +3,13 @@ import React from 'react'
 const QuoteItems = ( { img, text, altText} ) => {
 
     return (
-        <div className='quotes__items__item'>
-            <div className='imagewrapper'>
-                <img className="image" src={img} alt={altText}></img>
+        <div className="quotes__items__item">
+            <div className="quotes__items__item__imagewrapper">
+                <img src={img} alt={altText}></img>
             </div>
-            <div className="itemcontent">
-                <hr className="divider"></hr>
-                <div className="itemcontent__text">{text}</div>
+            <div className="quotes__items__item__content">
+                <hr className="quotes__items__item__content__divider"></hr>
+                <div className="quotes__items__item__content__text">{text}</div>
             </div>
         </div>
     )
