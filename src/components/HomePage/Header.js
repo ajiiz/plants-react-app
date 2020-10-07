c48a2969f00b73847b242430ff44b9d73f32ecea
ajiiz <opticpiter@gmail.com>
2020-10-07 12:45:05 +0200

    Added basic gsap animation in header element in Homepage component, now just for testing

--- src/components/HomePage/Header.js
+++ src/components/HomePage/Header.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import '../../styles/Home/header.scss'
+import { Power3, TimelineLite } from 'gsap'
 
 const Header = () => {
+    let container = useRef(null)
+    let tl = new TimelineLite
+    useEffect(() => {
+
+        const title = container.firstElementChild
+        const subTitle = container.children[1]
+        const divider = container.lastElementChild
+        tl.from(title, 2, {y: 500, ease: Power3.easeOut})
+        .from(subTitle, 2, {y: 500, ease: Power3.easeOut}, .6)
+        .from(divider, 2, {y: 500, ease: Power3.easeOut}, 1.2)
+    }, [])
+
     return (
         <header>
-            <div className="container">
+            <div className="container" ref={el => container = el}>
                 <h1 className="container__title">plant<span>s</span>pot</h1>
                 <h3 className="container__subtitle">healthy breath</h3>
                 <hr className="container__divider"></hr>
--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Quote from './Quote'
 import Header from './Header'
 import About from './About'
+import { TweenMax } from 'gsap'
 
 const Home = () => {
+    let home = useRef(null)
+    useEffect(() => {
+        TweenMax.to(home, 0, {css: {visibility: 'visible'}})
+    })
     return (
-        <div>
+        <div ref={el => home = el} className="wrapper">
             <Header />
             <Quote />
             <About />
