cb97cd6676fc35bb49a7f855032da028a5c6eba6
ajiiz <opticpiter@gmail.com>
2020-10-07 14:02:14 +0200

    Ended making header basic ease animation in Header component

--- src/components/HomePage/Header.js
+++ src/components/HomePage/Header.js
@@ -3,20 +3,22 @@ import '../../styles/Home/header.scss'
 import { Power3, TimelineLite } from 'gsap'
 
 const Header = () => {
+    /* Header Animation */
     let container = useRef(null)
-    let tl = new TimelineLite
+    let header = useRef(null)
+    let tl = new TimelineLite({delay: .4})
     useEffect(() => {
-
         const title = container.firstElementChild
         const subTitle = container.children[1]
         const divider = container.lastElementChild
-        tl.from(title, 2, {y: 500, ease: Power3.easeOut})
-        .from(subTitle, 2, {y: 500, ease: Power3.easeOut}, .6)
-        .from(divider, 2, {y: 500, ease: Power3.easeOut}, 1.2)
-    }, [])
+        tl.from(header, {duration: 2, opacity: 0, ease: Power3.easeInOut})
+        .from(title,  {duration: 1.5, opacity: 0, y: "-250", ease: Power3.easeOut}, 2.5)
+        .from(subTitle, {duration: 2, opacity: 0, x: "250", ease: Power3.easeInOut}, 3)
+        .from(divider,  {duration: 1.5, opacity: 0, x: "-250", ease: Power3.easeInOut}, 3.5)
+    }, [tl])
 
     return (
-        <header>
+        <header ref={el => header = el}>
             <div className="container" ref={el => container = el}>
                 <h1 className="container__title">plant<span>s</span>pot</h1>
                 <h3 className="container__subtitle">healthy breath</h3>
