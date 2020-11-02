1b75bab48d01e3c877f45b2f4d35d7ffe6ab014d
ajiiz <opticpiter@gmail.com>
2020-11-02 15:10:52 +0100

    Changed first question in Faq section from dummy to normal text

--- src/components/FaqPage/FaqItems.js
+++ src/components/FaqPage/FaqItems.js
@@ -1,7 +1,7 @@
 export const FaqItems = [
     {
-        question: "Add question?",
-        answer: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim."
+        question: "What are the benefits of indoor plants?",
+        answer: "Most importantly they improve the quality of your indoor air. Plants might also help reducing stress and boost your productivity. There are many more advantages but the best way is to find them out on your own!"
     },
     {
         question: "Add question?",
