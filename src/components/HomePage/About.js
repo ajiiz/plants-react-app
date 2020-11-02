bc7ee61bedf79e56efeeac631a5679dcc9408ee0
ajiiz <opticpiter@gmail.com>
2020-11-02 14:57:04 +0100

    Changed dummy text in about section to normal text

--- src/components/HomePage/About.js
+++ src/components/HomePage/About.js
@@ -9,10 +9,14 @@ const About = () => {
                 <h3 className="about__container__title">master the green</h3>
                 <hr className="about__container__divider"></hr>
                 <div className="about__container__text"><p>
-                A wonderful serenity has taken possession
-                of my entire
-                soul, like these sweet mornings of
-                spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now.
+                    There are many benefits of having plants in your room.
+                    Not only they might be a great decorative items but also
+                    have much more influence on your life. They improve state
+                    of being and lower your stress levels. May also help you
+                    solving sleeping problems. But most importantly they are
+                    natural air purifiers. There are many more advantages
+                    but the best way is to find it out yourself. Turn your
+                    room to a green paradise!
                 </p></div>
             </div>
         </div>
