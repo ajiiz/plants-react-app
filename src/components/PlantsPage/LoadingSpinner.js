c2ca8a140a22107a4146072a68add39ff61d0f96
ajiiz <opticpiter@gmail.com>
2020-11-03 14:46:24 +0100

    Added loading spinner that is shown when plants are loading in LoadingSpinner component

--- src/components/PlantsPage/LoadingSpinner.js
+++ src/components/PlantsPage/LoadingSpinner.js
@@ -2,9 +2,7 @@ import React from 'react'
 
 const LoadingSpinner = () => {
     return (
-        <div className="plants__container__items__spinner">
-            LOADING
-        </div>
+        <div className="plants__container__items__lds-dual-ring"></div>
     )
 }
 
